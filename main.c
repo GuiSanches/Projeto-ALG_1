d5e4ff4476492caed51a1122e1ad9bb146be27f2
Guilherme Queiroz <si.guilherme_queiroz@usp.br>
2018-10-09 14:18:29 -0300

    Primeiras ideias

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+int main() {
+	int n;//numero de brinquedos
+	int k;//intervalo
+	int p;//capacidade da caixa
+	obj_b brinquedo;
+
+	scanf("%d %d %d", &n, &k, &p);
+
+	cria_lista(&esteira);
+
+	for(int i = 0; i < n; i++){
+		scanf(" %s %s", brinquedo.nSerie, brinquedo.modelo);
+		insere_na_lista(&esteira, brinquedo);
+	}
+
+
+	return 0;
+}
+remove_da_lista(i);
+vazia_lista();
+
+
+cria_pilha();
+push(brinquedo);
+top();
+cheia_pilha();
+
